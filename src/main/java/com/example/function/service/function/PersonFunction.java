d590dbcd78609a8d05a3a3f2321f0be7c079373b
sruizvass <sergio.ruiz@vass.es>
2021-10-24 14:51:39 +0200

    Included functional example

--- src/main/java/com/example/function/service/function/PersonFunction.java
+++ src/main/java/com/example/function/service/function/PersonFunction.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 @Component(value = "person")
@@ -12,9 +16,27 @@ import java.util.function.Function;
 @AllArgsConstructor
 public class PersonFunction implements Function<String, Person> {
 
-    @Override
-    public Person apply(String s) {
-        log.info("finding person {}", s);
-        return new Person("Sergio", "Ruiz");
+  @Override
+  public Person apply(String s) {
+    log.info("finding person {}", s);
+    Optional<Person> person = findAllPersons()
+            .stream()
+            .filter(obj -> obj.getFirstName().equalsIgnoreCase(s))
+            .findFirst();
+    if(person.isEmpty()){
+      throw new RuntimeException("Person not found");
     }
+    return person.get();
+  }
+
+  private List<Person> findAllPersons() {
+    Person sergio = new Person("Sergio", "Ruiz");
+    Person andres = new Person("Andres", "Combariza");
+    Person dario = new Person("Dario", "Vives");
+    List<Person> personList = new ArrayList<>();
+    personList.add(sergio);
+    personList.add(andres);
+    personList.add(dario);
+    return  personList;
+  }
 }
